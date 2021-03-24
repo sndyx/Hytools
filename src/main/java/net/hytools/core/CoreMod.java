206550612d086f993aa6ca87d0135e63d71fda17
Sandy <70289658+sndyx@users.noreply.github.com>
2021-03-24 11:31:54 -0400

    Update CoreMod.java

--- src/main/java/net/hytools/core/CoreMod.java
+++ src/main/java/net/hytools/core/CoreMod.java
@@ -1,1 +1,12 @@
+package net.hytools.core;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "hytools", version = 1.0) public class CoreMod {
+
+	@Mod.EventHandler public void init(FMLInitializationEvent event) {
+		
+	}
+
+}
