9542d3bf4661bf1debab7fc05296b86609716df4
Sandy <70289658+sndyx@users.noreply.github.com>
2021-03-24 11:14:51 -0400

    Delete src/main/java/moe directory

--- src/main/java/moe/sndy/hytools/core/Resources.java
+++
@@ -1,9 +0,0 @@
-package moe.sndy.hytools.core;
-
-public final class Resources {
-
-    public static final String MODID = "hytools";
-    public static final String VERSION = "3.0";
-    public static String API_KEY = "87f957a6-bc42-4617-a523-151d57f936ad";
-
-}
